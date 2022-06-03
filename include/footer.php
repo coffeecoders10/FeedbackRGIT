23154e3a16222e56de230bc3b67789b149e9bc4c
Nitish <ntaleka@ncsu.edu>
2022-06-03 09:26:40 -0500

    init

---
+++ include/footer.php
@@ -0,0 +1,18 @@
+<style media="screen">
+.admin_nav{
+  text-transform: uppercase;
+  font-family: Montserrat-SemiBold;
+}
+</style>
+<footer class="py-2 bg-dark">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-8">
+          <p class="m-0 text-white admin_nav" style="font-size:10">This feedback is for process of continuous improvement. Faculty member can make action plan based on this feedback. Developed by: Nitish Talekar, Sarvesh Wanode, Aayush Singh</p>
+        </div>
+        <div class="col-4">
+          <p class="m-0 text-right text-white admin_nav" style="font-size:10">MCT_RGIT_IQAC/feedback/even_semester/2021</p>
+        </div>
+      </div>
+    </div>
+</footer>
